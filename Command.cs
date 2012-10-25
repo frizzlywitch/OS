5c4ad769878b2985d0603fbabd3363376af9c78d
U-Marina-PC\Marina <frizzlywitch@gmail.com>
2012-10-25 23:41:09 +0600

    Processor main functionality

---
+++ Command.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Processor.Vol2
+{
+    class Command
+    {
+        public byte OpCode;
+        public byte I;
+        public byte P;
+        public byte Op;
+
+        public virtual byte Pereh(byte prznk)
+        {
+            return 0;
+        }
+    }
+
+    class Branch : Command
+    {
+         public override byte Pereh(byte prznk)
+         {
+             return (byte) 1;
+         }
+    }
+
+    class BranchZero : Command
+    {
+        public override byte Pereh(byte prznk)
+        {
+            return (byte) (prznk == 0 ? 1 : 0);
+        }
+    }
+
+    class BranchNotZero : Command
+    {
+        public override byte Pereh(byte prznk)
+        {
+            return (byte) (prznk != 0 ? 1 : 0);
+        }
+    }
+}
