4e582d6c05403a5de903eb436880208dbcb3af72
Kamalova Marina <frizzlywitch@gmail.com>
2013-05-13 01:21:16 +0600

    Scheduler fixed bugs and some remarks

--- Scheduler/scheduler.c
+++ Scheduler/scheduler.c
@@ -5,29 +5,39 @@
 #include <stdio.h>
 #include <string.h>
 
-ucontext_t* context[2];
+ucontext_t context[2];
 int cont_num = 0;
-ucontext_t* scheduler_context;
+ucontext_t scheduler_context;
+
+int cmp_context(ucontext_t* c1, ucontext_t* c2)
+{
+    int i;
+    char* x1 = (char*) c1;
+    char* x2 = (char*) c2;
+    for (i = 0; i < sizeof(ucontext_t); ++i)
+    {
+        if (x1[i] != x2[i])
+            return 0;
+    }
+    return 1;
+}
 
 void sheduler(ucontext_t* rem_context)
 {   
-    printf("Scheduler start\n");
-
-    context[cont_num] = rem_context;
+    context[cont_num] = *rem_context;
     cont_num ^= 1;
-    ucontext_t* new_context = context[cont_num];
-
-    if(new_context == NULL)
+    ucontext_t* new_context = &context[cont_num];
+    if(cmp_context(new_context, &scheduler_context))
     {
-        if(rem_context == NULL)
+        if(cmp_context(rem_context, &scheduler_context))
+        {
             exit(0);
-
+        }
         new_context = rem_context;
         cont_num ^= 1;
     }
 
     alarm(1);
-    printf("Try set context %d\n", cont_num);
     setcontext(new_context);
 }
 
@@ -50,38 +60,37 @@ void factorial(int number)
 
 void make_processes()
 {
-    context[0] = (ucontext_t*) calloc(1, sizeof(ucontext_t));
-    context[1] = (ucontext_t*) calloc(1, sizeof(ucontext_t));
+    memset(context, 0, 2 * sizeof(ucontext_t));
     void* stack1 = calloc(4096, sizeof(char));
     void* stack2 = calloc(4096, sizeof(char));
     
-    if(context[0] == NULL || context[1] == NULL || stack1 == NULL || stack2 == NULL)
+    if(stack1 == NULL || stack2 == NULL)
     {
         printf("Memory allocation failed\n");
         abort();
     }
 
-    getcontext(context[0]);
-    context[0]->uc_stack.ss_sp = stack1;
-    context[0]->uc_stack.ss_size = 4096;
-    context[0]->uc_link = scheduler_context;
-    makecontext(context[0], (void(*)(void))factorial, 1, 100);
+    getcontext(&context[0]);
+    context[0].uc_stack.ss_sp = stack1;
+    context[0].uc_stack.ss_size = 4096;
+    context[0].uc_link = &scheduler_context;
+    makecontext(&context[0], (void(*)(void))factorial, 1, 100);
 
-    getcontext(context[1]);
-    context[1]->uc_stack.ss_sp = stack2;
-    context[1]->uc_stack.ss_size = 4096;
-    context[1]->uc_link = scheduler_context;
-    makecontext(context[1], (void(*)(void))factorial, 1, 100);
+    getcontext(&context[1]);
+    context[1].uc_stack.ss_sp = stack2;
+    context[1].uc_stack.ss_size = 4096;
+    context[1].uc_link = &scheduler_context;
+    makecontext(&context[1], (void(*)(void))factorial, 1, 100);
 }
 
 void make_sheduler()
 {
-    scheduler_context = (ucontext_t*) calloc(1, sizeof(ucontext_t));
-    getcontext(scheduler_context);
+    memset(&scheduler_context, 0, sizeof(ucontext_t));
+    getcontext(&scheduler_context);
     void* stack = calloc(4096, sizeof(char));
-    scheduler_context->uc_stack.ss_sp = stack;
-    scheduler_context->uc_stack.ss_size = 4096;
-    makecontext(scheduler_context, (void(*)(void))sheduler, 0);
+    scheduler_context.uc_stack.ss_sp = stack;
+    scheduler_context.uc_stack.ss_size = 4096;
+    makecontext(&scheduler_context, (void(*)(void))sheduler, 0);
 }
 
 void alarm_handler(int n, siginfo_t* sinfo, void* context)
@@ -103,6 +112,6 @@ int main()
     make_sheduler();
     make_processes();
     set_handler_for_alarm_action();
-    sheduler(context[0]);
+    sheduler(&context[0]);
     return 0;
 }
